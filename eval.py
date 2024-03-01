534ce70f5d0bc7c1220a8a538fe08bde9a49cecf
MikeyBrockey <brockman.michael2003@gmail.com>
2024-03-01 11:33:40 -0800

    data processing for eval

--- eval.py
+++ eval.py
@@ -1,19 +1,51 @@
 import os
-from nltk.translate import bleu_score
-from rouge_metric import PyRouge
-from bert_score import score
+# from nltk.translate import bleu_score
+# from rouge_metric import PyRouge
+# from bert_score import score
+
+def read_txt_files_into_dict(directory):
+    # Dictionary to hold contents of txt files from each directory
+    content_dict = {}
+    
+    for filename in os.listdir(directory):
+        # Check if the file is a .txt file
+        if filename.endswith('.txt'):
+            # Construct the full file path
+            filepath = os.path.join(directory, filename)
+            # Read the content of the file
+            with open(filepath, 'r') as file:
+                content = file.read()
+                # Use filename without extension as key
+                key = os.path.splitext(filename)[0]
+                content_dict[key] = content
+    else:
+        print(f"Directory {directory} does not exist.")
+    
+    return content_dict
+
 
 def load_data():
-    test_data_folder = patient_data_folder = ("./processed_data/test")
+    meditron_dict = read_txt_files_into_dict("./generated_outputs/test")
+    oracle_dict = read_txt_files_into_dict("./oracle/test")
+    
+    meditron_list = []
+    oracle_list = []
 
-def bleu():
+    overlap = list(set(meditron_dict.keys()).intersection(set(oracle_dict.keys())))
+    for key in overlap:
+        meditron_list.append(meditron_dict[key])
+        oracle_list.append(oracle_dict[key])
+    
+    return oracle_list, meditron_list
+
+def bleu(refs, hyps):
     '''
     list_of_references = [[ref1a, ref1b, ref1c], [ref2a]]
     hypotheses = [hyp1, hyp2]
     '''
     bleu = bleu_score.corpus_bleu(refs, hyps)
 
-def rouge():
+def rouge(refs, hyps):
     '''
     hypotheses = [
     'how are you\ni am fine',  # document 1: hypothesis
@@ -29,11 +61,11 @@ def rouge():
     '''
     rouge = PyRouge(rouge_n=(1, 2, 4), rouge_l=True, rouge_w=True,
                 rouge_w_weight=1.2, rouge_s=True, rouge_su=True, skip_gap=4)
-    scores = rouge.evaluate(generated, oracle)
+    scores = rouge.evaluate(hyps, refs)
 
-def bertscore():
+def bertscore(refs, hyps):
     # 
-    P, R, F1 = score(cands, refs, lang='en', verbose=True)
+    P, R, F1 = score(hyps, refs, lang='en', verbose=True)
 
 if __name__ == "__main__":
-    pass
+    oracle_list, meditron_list = load_data()
