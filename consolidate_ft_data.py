36fa89ec39a62f5b9779080ccfe6fe00fd575e97
hhekmat <hahekmat777@gmail.com>
2024-03-12 04:24:21 -0700

    quick script to consolidate jsonls into ft datasets

--- consolidate_ft_data.py
+++ consolidate_ft_data.py
@@ -1,20 +1,19 @@
 import os
+import json
 
-
+def create_ft_datasets(inp, outp):
+    for i in range(len(inp)):
+        with open(outp[i], 'a') as outfile:
+            for root, dirs, files in os.walk(inp[i]):
+                for file in files:
+                    if file.endswith('.jsonl'):
+                        file_path = os.path.join(root, file)
+                        with open(file_path, 'r') as infile:
+                            for line in infile:
+                                outfile.write(line)
+        
 
 if __name__ == '__main__':
-    # Accepting task number as a command-line argument
-    if len(sys.argv) > 1:
-        task = int(sys.argv[1])
-        if task == 1:
-            asyncio.run(process_task_1())
-        # elif task == 3: call process_task_3()
-        elif task == 2:
-            populate_global_resources("./mock_patients")
-            asyncio.run(process_task_2())
-        elif task == 3: 
-            process_task_3()
-        else:
-            print("Invalid task number. Please choose 1, 2, or 3.")
-    else:
-        print("Please provide a task number as a command-line argument.")
+    input_dirs = ['./task_2/finetune/oracle/train', './task_2/finetune/oracle/validation', './task_3/finetune/oracle/train', './task_3/finetune/oracle/validation']
+    output_files = ['./ft_datasets/task_2_train.jsonl', './ft_datasets/task_2_val.jsonl', './ft_datasets/task_3_train.jsonl', './ft_datasets/task_3_val.jsonl']
+    create_ft_datasets(input_dirs, output_files)
