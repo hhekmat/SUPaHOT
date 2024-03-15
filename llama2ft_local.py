7db22ab0a16b8d03682d06c6a44fefac6fea2f46
hhekmat <hahekmat777@gmail.com>
2024-03-14 18:30:42 -0700

    fixing local finetune output generation script prompt

--- llama2ft_local.py
+++ llama2ft_local.py
@@ -12,98 +12,24 @@ api_token = os.getenv('TOGETHER_API_KEY')
 API_URL = "https://api-inference.huggingface.co/models/meta-llama/Llama-2-7b-hf"
 headers = {f"Authorization": {api_token}}
 
-
-
-'''# Initialize the LLaMA model and tokenizer for the chat model
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
-generate = pipeline("text-generation", model=model, tokenizer=tokenizer, device=-1)  # Adjust 'device' as per your setup'''
-
-'''def giveup_condition(details):
-    """Condition to give up retrying, for example when status code is not 429."""
-    exc = details['value']
-    # Assuming 'exc' is the raised requests.exceptions.HTTPError
-    if exc.response.status_code != 429:
-        return True
-    return False
-
-@backoff.on_exception(
-    backoff.expo,
-    requests.exceptions.HTTPError,  # Retrying on HTTP errors
-    giveup=giveup_condition,  # Custom giveup condition
-    max_time=300,  # Total max time to retry
-    jitter=backoff.full_jitter,  # Adding jitter to spread out retry attempts
-)'''
-
 def generate_llama_response(user_prompt, task_prompt):
     # Encode the input context
-    prompt = f"[INST] You are a helpful medical assistant. Users ask you questions about their health care information. You will help and be as concise and clear as possible.\n\n{task_prompt}\n\n{user_prompt}\n[/INST]"
+    prompt = f"<s>[INST] <<SYS>> You are a helpful medical assistant. Users ask you questions about their health care information. You will help and be as concise and clear as possible. {task_prompt} <</SYS>> {user_prompt} [/INST]"
 
     input_ids = tokenizer.encode(prompt, return_tensors="pt")
     
     # Generate the output sequence
-    output = model.generate(input_ids.to(device), max_length=128, temperature=0.7).cpu()
+    output = model.generate(input_ids.to(device), max_new_tokens=100, temperature=0.01).cpu()
     
     # Decode and return the output text, skipping special tokens
     output_text = tokenizer.decode(output[0], skip_special_tokens=True)
     return output_text
-
-'''def generate_llama_response(user_prompt, task_prompt, model):
-    endpoint = 'https://api.together.xyz/v1/chat/completions'
-    headers = {
-        "Authorization": "Bearer f4a39e6ede5141b23e5ba2febeb4428589a55db917e72cbe5a4234e3a16a669b"
-    }
-
-    try:
-        input_text = f"[INST] You are a helpful medical assistant. Users ask you questions about their health care information. You will help and be as concise and clear as possible.\n\n{task_prompt}\n\n{user_prompt}\n[/INST]"
-        payload = {
-            "model": model,
-            "max_tokens": 2048,
-            "prompt": input_text,
-            "temperature": 0.01,
-            "top_p": 0.7,
-            "top_k": 50,
-            "repetition_penalty": 1,
-            "stop": ["[/INST]", "</s>"],
-            "repetitive_penalty": 1
-        }
-        
-        response = query(endpoint, headers, payload)
-        if response and 'choices' in response and response['choices']:
-            return response['choices'][0]['message']['content'].strip()
-        else:
-            return "No response generated."
-    except Exception as e:
-        print(f"Error during API call: {e}")
-        return None
-
-def query(endpoint, headers, payload):
-    response = requests.post(endpoint, headers=headers, json=payload)
-    try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as e:
-        if response.status_code == 429:
-            retry_after = response.headers.get("Retry-After")
-            wait_time = int(retry_after) if retry_after else 10  # Default to 30 seconds if header is missing
-            print(f"Hit rate limit, retrying after {wait_time} seconds...")
-            time.sleep(wait_time)  # Sleep for the time specified in the Retry-After header
-            return query(endpoint, headers, payload)  # Recursive retry after waiting
-        else:
-            print(f"Error during API call: {e}")
-            raise
-    return response.json()'''
-
-'''def query(endpoint, headers, payload):
-    response = requests.post(endpoint, headers=headers, json=payload)
-    response.raise_for_status()
-    return response.json()'''
 
 
 # Adapting the file processing functions for task 1 and task 2
 def process_task_1():
     base_dir = 'queries'
     output_dir = 'task_1/output/llama_ft'
-    relevant_data_dir = 'all_resources'
 
     task_1_prompt = "Given a query and a resource from a patient's medical record, your job is to determine if the resource could potentially be relevant to providing an answer to the patient's query about their medical history. Respond only with 'True' if the resource may be relevant, or 'False' if the resource would not be helpful at all in providing the patient an answer to their question." 
     for root, dirs, files in os.walk(base_dir):
@@ -115,34 +41,29 @@ def process_task_1():
 
                 query = lines[0].strip()
                 resources = lines[1:]
-                stripped_filename = re.sub(r'\d+', '', file.split('.')[0])
-                relevant_data_file = os.path.join(relevant_data_dir, f"{stripped_filename}resources.txt")
+                print(query)
+                relevant_resources = []
+                c = 0
+                for resource in resources:
+                    c += 1
+                    print(c)
+                    resource = resource.strip()
+                    prompt = f"Query: {query}, resource: {resource}"
 
-                if os.path.exists(relevant_data_file):
-                    relevant_resources = []
-                    c = 0
-                    output_subdir = os.path.join(output_dir, os.path.relpath(root, base_dir))
-                    if output_subdir.find('test') != -1:
-                        for resource in resources:
-                            c += 1
-                            print(c)
-                            resource = resource.strip()
-                            prompt = f"Query: {query}, resource: {resource}"
-    
-                            llama_response = generate_llama_response(prompt, task_1_prompt)
-                            print(llama_response)
-                            if llama_response.find("True") != -1:
-                                print('true')
-                                relevant_resources.append(resource)
+                    llama_response = generate_llama_response(prompt, task_1_prompt)
+                    print(llama_response)
+                    if llama_response.find("True") != -1:
+                        print('true')
+                        relevant_resources.append(resource)
 
-                    output_subdir = os.path.join(output_dir, os.path.relpath(root, base_dir))
-                    os.makedirs(output_subdir, exist_ok=True)
-                    output_file = os.path.join(output_subdir, file)
+                output_subdir = os.path.join(output_dir, os.path.relpath(root, base_dir))
+                os.makedirs(output_subdir, exist_ok=True)
+                output_file = os.path.join(output_subdir, file)
 
-                    with open(output_file, 'w') as f:
-                        f.writelines("%s\n" % resource for resource in relevant_resources)
+                with open(output_file, 'w') as f:
+                    f.writelines("%s\n" % resource for resource in relevant_resources)
 
-                    print(f'Processed {file_path} -> {output_file}')
+                print(f'Processed {file_path} -> {output_file}')
 
 def process_task_2():
     base_dir = 'task_1/output/oracle'
@@ -242,8 +163,9 @@ if __name__ == '__main__':
             tokenizer = AutoTokenizer.from_pretrained(f"./ft_model/post_task_{task}")
             model = AutoModelForCausalLM.from_pretrained(
                 f"./ft_model/post_task_{task}",
+                load_in_4bit=True,
                 trust_remote_code=True,
-            ).to(device)
+            )
             process_task_1()
         elif task == 2:
             # Ensure any global resources are populated if necessary
@@ -253,6 +175,7 @@ if __name__ == '__main__':
             tokenizer = AutoTokenizer.from_pretrained(f"./ft_model/post_task_{task}")
             model = AutoModelForCausalLM.from_pretrained(
                 f"./ft_model/post_task_{task}",
+                load_in_4bit=True,
                 trust_remote_code=True,
             ).to(device)
             process_task_2()
@@ -262,6 +185,7 @@ if __name__ == '__main__':
             tokenizer = AutoTokenizer.from_pretrained(f"./ft_model/post_task_{task}")
             model = AutoModelForCausalLM.from_pretrained(
                 f"./ft_model/post_task_{task}",
+                load_in_4bit=True,
                 trust_remote_code=True,
             ).to(device)
             process_task_3()
